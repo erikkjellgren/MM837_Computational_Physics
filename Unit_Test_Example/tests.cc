96fa669a6976429e3edbc33de5bb61c3974f3f8e
Melisius <erikkjellgren@live.dk>
2017-09-10 17:14:46 +0200

    Fixed mistake in code

--- Unit_Test_Example/tests.cc
+++ Unit_Test_Example/tests.cc
@@ -5,20 +5,20 @@
 TEST(Quadratic, simpletest){
 	std::vector<double>roots;
 	roots = quadraticroot(1, 0, 0);
-	ASSERT_EQ (roots[0], 0.0);
+	ASSERT_EQ (0.0, roots[0]);
 }
 
 TEST(Quadratic, ManyTests){
 	std::vector<double>roots;
 	roots = quadraticroot(1, 1, -4);
-	ASSERT_NEAR (roots[0],1.56155281281, 1e-10);
-	ASSERT_NEAR (roots[1],-2.56155281281, 1e-10);
+	ASSERT_EQ (1.56155281281, roots[0]);
+	ASSERT_EQ (-2.56155281281, roots[1]);
 	roots = quadraticroot(1, -3, -4);
-	ASSERT_EQ (roots[0],4);
-	ASSERT_EQ (roots[1],-1);
+	ASSERT_EQ (4, roots[0]);
+	ASSERT_EQ (-1, roots[1]);
 	roots = quadraticroot(1, 0, -4);
-	ASSERT_EQ (roots[0],2);
-	ASSERT_EQ (roots[1],-2);
+	ASSERT_EQ (2, roots[0]);
+	ASSERT_EQ (-2, roots[1]);
 }
 
 int main(int argc, char **argv) {
